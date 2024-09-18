b15a603711dd749bd8ad27a1ee3376a1c14dc416
rony31416 <ronymajumder1171@gmail.com>
2024-09-18 10:11:36 +0600

    NEW

--- API creation/routing.js
+++ API creation/routing.js
@@ -5,10 +5,12 @@
 
 const {samplehandler} = require('./handlers/routinghandlers/samplehandler');
 const {userhandler} = require('./handlers/routinghandlers/userHandler');
+const {tokenhandler} = require('./handlers/routinghandlers/tokenHandler');
 
 const routers = {
     'sample' : samplehandler,
     'user' : userhandler,
+    'token' : tokenhandler,
 };
 
 module.exports = routers;
